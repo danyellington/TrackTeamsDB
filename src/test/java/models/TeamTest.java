f222429791b718e872d54682f923db3f113b5b0b
Danyel Murray <danyelmurray1987@gmail.com>
2018-01-12 15:37:53 -0800

    rerun tests

--- src/test/java/models/TeamTest.java
+++ src/test/java/models/TeamTest.java
@@ -19,49 +19,49 @@ public class TeamTest {
         Team.clearAllTeams();
     }
 
-
-    @Test
-    public void createTeam_team_getTeam() throws Exception {
-        Team testTeam = new Team("Silver Snakes", "cool", "Bertie");
-        assertEquals(true, testTeam instanceof Team);
-    }
-
-    @Test
-    public void TeamName_getName_getTeamName() throws Exception {
-        Team testTeam = new Team("Silver Snakes", "cool", "Bertie");
-        assertEquals("Silver Snakes", testTeam.getTeamName());
-    }
-
-    @Test
-    public void TeamDescription_getDescription_getTeamDescription() throws Exception {
-        Team testTeam = new Team("Silver Snakes", "cool", "Bertie");
-        assertEquals("cool", testTeam.getDescription());
-    }
-
-    @Test
-    public void TeamMemberName_getMemberName_getTeamMemberName() throws Exception {
-        Team testTeam = new Team("Silver Snakes", "cool", "Bertie");
-        assertEquals("Bertie", testTeam.getMemberName());
-    }
+//
+//    @Test
+//    public void createTeam_team_getTeam() throws Exception {
+//        Team testTeam = new Team("Silver Snakes", "cool", "Bertie");
+//        assertEquals(true, testTeam instanceof Team);
+//    }
+//
+//    @Test
+//    public void TeamName_getName_getTeamName() throws Exception {
+//        Team testTeam = new Team("Silver Snakes", "cool", "Bertie");
+//        assertEquals("Silver Snakes", testTeam.getTeamName());
+//    }
+//
+//    @Test
+//    public void TeamDescription_getDescription_getTeamDescription() throws Exception {
+//        Team testTeam = new Team("Silver Snakes", "cool", "Bertie");
+//        assertEquals("cool", testTeam.getDescription());
+//    }
+//
+//    @Test
+//    public void TeamMemberName_getMemberName_getTeamMemberName() throws Exception {
+//        Team testTeam = new Team("Silver Snakes", "cool", "Bertie");
+//        assertEquals("Bertie", testTeam.getMemberName());
+//    }
 
     @Test
     public void testTeam_instantiatesCorrectly_true() {
         Team testTeam = setupNewTeam();
         assertEquals(true, testTeam instanceof Team);
     }
-
-    @Test
-    public void testTeam_correctlyReturned_true() {
-        Team testTeam = setupNewTeam();
-        assertTrue(Team.getAll().contains(testTeam));
-
-    }
-
-    @Test
-    public void testOtherTeam_correctlyReturned_true() {
-        Team otherTeam = new Team("Green Monkeys", "neat", "Gertrude");
-        assertTrue(Team.getAll().contains(otherTeam));
-    }
+//
+//    @Test
+//    public void testTeam_correctlyReturned_true() {
+//        Team testTeam = setupNewTeam();
+//        assertTrue(Team.getAll().contains(testTeam));
+//
+//    }
+//
+//    @Test
+//    public void testOtherTeam_correctlyReturned_true() {
+//        Team otherTeam = new Team("Green Monkeys", "neat", "Gertrude");
+//        assertTrue(Team.getAll().contains(otherTeam));
+//    }
 
     public Team setupNewTeam() {
         return new Team("Silver Snakes", "cool", "Bertie");
